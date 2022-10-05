f924a81f0582d69f556bb8118be2ab37bf3b8520
Jeremiah Norris <jjno91@gmail.com>
2022-10-05 15:14:19 -0500

    remove shell parameters

--- main.sh
+++ main.sh
@@ -1,7 +1,10 @@
-#!/bin/bash -ex
+#!/bin/bash
 
 # preflight checks
-aws sts get-caller-identity > /dev/null || echo "AWS Authentication Failed" && exit 1
+aws sts get-caller-identity > /dev/null || echo "ERROR authenticating with AWS" && exit 1
+git --version || echo "ERROR missing dependency: git" && exit 2
+terraform --version || echo "ERROR missing dependency: terraform" && exit 3
+aws --version || echo "ERROR missing dependency: aws" && exit 4
 
 export DIRECTORY=${DIRECTORY:-terraform}
 export BACKEND_PREFIX=${BACKEND_PREFIX:-terraform-backend}
@@ -13,8 +16,13 @@ fi
 
 if [ -z "${BUCKET}" ]; then
     BUCKET=$(aws s3 ls | cut -d " " -f 3 | grep -e "^${BACKEND_PREFIX}")
+    export BUCKET
+fi
+
+if [ -z "${BUCKET}" ]; then
+    echo "cannot find bucket"
 fi
 
 if [ -d "${DIRECTORY}" ]; then
-    cd "${DIRECTORY}"
+    cd "${DIRECTORY}" || echo "ERROR changing directory to: ${DIRECTORY}" && exit 2
 fi
