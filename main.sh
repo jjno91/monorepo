4a9ce3c80b6b9b70edce27f5e506dbb2f9896061
Jeremiah Norris <jjno91@gmail.com>
2022-10-05 14:49:27 -0500

    select s3 bucket based on prefix

--- main.sh
+++ main.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -ex
 
-export DEFAULT_BRANCH=${DEFAULT_BRANCH:-main}
 export DIRECTORY=${DIRECTORY:-terraform}
+export BACKEND_PREFIX=${BACKEND_PREFIX:-terraform-backend}
 
 if [ -z "${BRANCH}" ]; then
     BRANCH=$(git branch --show-current)
     export BRANCH
+fi
+
+if [ -z "${BUCKET}" ]; then
+    BUCKET=$(aws s3 ls | cut -d " " -f 3 | grep -e "^${BACKEND_PREFIX}")
 fi
 
 if [ -d "${DIRECTORY}" ]; then
