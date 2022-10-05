851131faa8c031b04569db673f57dbdde0a683af
Jeremiah Norris <jjno91@gmail.com>
2022-10-05 14:24:17 -0500

    detect git branch and support directory targeting

--- main.sh
+++ main.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -ex
 
-echo hello
+export DEFAULT_BRANCH=${DEFAULT_BRANCH:-main}
+export DIRECTORY=${DIRECTORY:-terraform}
+
+if [ -z "${BRANCH}" ]; then
+    BRANCH=$(git branch --show-current)
+    export BRANCH
+fi
+
+if [ -d "${DIRECTORY}" ]; then
+    cd "${DIRECTORY}"
+fi
